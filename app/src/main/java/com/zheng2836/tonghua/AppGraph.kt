c07bf4c2faa31ecc9bd268c6ebf9f6750d4fd00e
z404hr <120444868+zheng2836@users.noreply.github.com>
2026-04-20 05:08:05 +0800

    add app graph for shared state

---
+++ app/src/main/java/com/zheng2836/tonghua/AppGraph.kt
@@ -0,0 +1,32 @@
+package com.zheng2836.tonghua
+
+import android.content.Context
+import com.zheng2836.tonghua.data.CallStore
+import com.zheng2836.tonghua.signaling.SignalingClient
+import com.zheng2836.tonghua.telecom.ConnectionRegistry
+import com.zheng2836.tonghua.webrtc.WebRtcEngine
+
+object AppGraph {
+    lateinit var appContext: Context
+        private set
+
+    lateinit var callStore: CallStore
+        private set
+
+    lateinit var connectionRegistry: ConnectionRegistry
+        private set
+
+    lateinit var signalingClient: SignalingClient
+        private set
+
+    lateinit var webRtcEngine: WebRtcEngine
+        private set
+
+    fun init(context: Context) {
+        appContext = context.applicationContext
+        callStore = CallStore()
+        connectionRegistry = ConnectionRegistry()
+        signalingClient = SignalingClient(appContext, callStore, connectionRegistry)
+        webRtcEngine = WebRtcEngine(callStore)
+    }
+}
