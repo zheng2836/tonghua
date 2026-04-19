19e338ed91ee8c84d95cdb2534848792254d1a39
z404hr <120444868+zheng2836@users.noreply.github.com>
2026-04-20 05:10:41 +0800

    wire app graph on startup

--- app/src/main/java/com/zheng2836/tonghua/App.kt
+++ app/src/main/java/com/zheng2836/tonghua/App.kt
@@ -6,6 +6,8 @@ import com.zheng2836.tonghua.telecom.PhoneAccountRegistrar
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppGraph.init(this)
+        AppGraph.signalingClient.connect()
         PhoneAccountRegistrar.registerIfNeeded(this)
     }
 }
