4d77e649f7a28823ec027a2f8c3f9a91d18bdcc6
z404hr <120444868+zheng2836@users.noreply.github.com>
2026-04-20 09:11:52 +0800

    avoid startup crash by deferring telecom registration and socket connect

--- app/src/main/java/com/zheng2836/tonghua/App.kt
+++ app/src/main/java/com/zheng2836/tonghua/App.kt
@@ -1,13 +1,10 @@
 package com.zheng2836.tonghua
 
 import android.app.Application
-import com.zheng2836.tonghua.telecom.PhoneAccountRegistrar
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         AppGraph.init(this)
-        AppGraph.signalingClient.connect()
-        PhoneAccountRegistrar.registerIfNeeded(this)
     }
 }
